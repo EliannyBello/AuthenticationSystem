e975354beb4011ed4edf8d72dbb5f90f603643ce
EliannyBello <elibellomanzo@gmail.com>
2024-11-04 23:56:09 -0300

    empezado

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    profile = db.relationship('Profile', backref="user", uselist=False)
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_active": self.is_active,
+            "profile": self.profile.serialize() if self.profile else None
         }
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    biography = db.Column(db.String, default="")
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "biography": self.biography
+        }
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,22 +1,57 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
-from flask_cors import CORS
+import datetime
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import User, Profile
+from extensions import db
 
-api = Blueprint('api', __name__)
+bp_auth = Blueprint("bp_auth", __name__)
 
-# Allow CORS requests to this API
-CORS(api)
-
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+@bp_auth.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    if not email:
+        return jsonify({"status": "error", "message": "Email is required"}), 422
+    if not password:
+        return jsonify({"status": "error", "message": "Password is required"}), 422
+    found = User.query.filter_by(email=email).first()
+    if not found or not check_password_hash(found.password, password):
+        return jsonify({"status": "error", "message": "Email/password are incorrect"}), 401
+    expire = datetime.timedelta(days=5)
+    access_token = create_access_token(identity=found.id, expires_delta=expire)
+    data = {
+        "access_token": access_token,
+        "user": found.serialize()
     }
+    return jsonify({"status": "success", "message": "Login successfully", "data": data}), 200
 
-    return jsonify(response_body), 200
+
+
+
+@bp_auth.route('/register', methods=['POST'])
+def register():
+    username = request.json.get('username')
+    email = request.json.get('email')
+    password = request.json.get('password')
+    if not username:
+        return jsonify({"status": "error", "message": "Username is required"}), 422
+    if not email:
+        return jsonify({"status": "error", "message": "Email is required"}), 422
+    if not password:
+        return jsonify({"status": "error", "message": "Password is required"}), 422
+    if User.query.filter_by(email=email).first():
+        return jsonify({"status": "error", "message": "Email is already in use!"}), 422
+    if User.query.filter_by(username=username).first():
+        return jsonify({"status": "error", "message": "Username is already in use!"}), 422
+    profile = Profile()
+    user = User(username=username, email=email, password=generate_password_hash(password), profile=profile)
+    db.session.add(user)
+    db.session.commit()
+    expire = datetime.timedelta(days=5)
+    access_token = create_access_token(identity=user.id, expires_delta=expire)
+    data = {
+        "access_token": access_token,
+        "user": user.serialize()
+    }
+    return jsonify({"status": "success", "message": "Register successfully", "data": data}), 200
